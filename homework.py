c63bdec2c1b6ddfa5f1672c6616d5ce3d11ff909
Zachary Daniels <zachdaniels@Zacharys-MacBook-Air.local>
2024-04-21 18:44:27 -0700

    update code

--- homework.py
+++ homework.py
@@ -10,10 +10,25 @@ class MLP:
         # Initialize weights and biases
         self.weights = []
         self.biases = []
-        self.weights.append(np.random.rand(self.hidden_nodes[0], self.input_size))
-        for i in range(1, hidden_layers):
-            self.weights.append(np.random.rand(self.hidden_nodes[i], self.hidden_nodes[i - 1]))
-            self.biases.append(np.random.rand(self.hidden_nodes[i], 1))
+        self.weights.append(np.random.rand(self.input_size, self.hidden_nodes[0]))
+        self.biases.append(np.random.rand(1, self.hidden_nodes[0]))
+        for i in range(hidden_layers - 1):
+            self.weights.append(np.random.rand(self.hidden_nodes[i], self.hidden_nodes[i + 1]))
+            self.biases.append(np.random.rand(1, self.hidden_nodes[i]))
+        self.weights.append(np.random.rand(self.hidden_nodes[-1], self.output_size))
+        self.biases.append(np.random.rand(1, self.hidden_nodes[-1]))
+
+    def print_weights(self):
+        for i in range(len(self.weights)):
+            w = np.array(self.weights[i])
+            print(w.shape)
+
+    def print_biases(self):
+        for i in range(len(self.biases)):
+            b = np.array(self.biases[i])
+            if i == 0:
+                print(self.biases[0])
+            print(b.shape)
 
     def forward_propagation(self, x):
         activations = [x]
@@ -21,16 +36,20 @@ class MLP:
             z = np.dot(self.weights[i], activations[-1]) + self.biases[i]
             a = self.relu(z)
             activations.append(a)
+        weight = np.array(self.weights[-1])
         output = np.dot(self.weights[-1], activations[-1]) + self.biases[-1]
         return output, activations
 
     def backpropagation(self, x, y, output, activations, learning_rate):
         delta = output - y
+        print(output)
         for i in range(self.hidden_layers, -1, -1):
             dz = delta if i == self.hidden_layers else np.dot(self.weights[i + 1].T, delta) * self.relu_derivative(activations[i + 1])
             dw = np.dot(dz, activations[i].T)
             db = dz
-            print(len(self.weights), i)
+            w = np.array(self.weights[i])
+            deltaw = np.array(dw)
+            print(w.shape, deltaw.shape, i)
             self.weights[i] -= learning_rate * dw
             self.biases[i] -= learning_rate * db
             delta = dz
@@ -66,15 +85,17 @@ learning_rate = 0.01
 epochs = 1000
 
 mlp = MLP(input_size, hidden_layers, hidden_nodes, output_size)
+mlp.print_weights()
+mlp.print_biases()
 
 # Generate random input data and labels
 X = np.random.rand(input_size, 100)
 y = np.random.rand(output_size, 100)
 
 # Train the MLP
-mlp.train(X, y, epochs, learning_rate)
+# mlp.train(X, y, epochs, learning_rate)
 
 # Make predictions
 test_X = np.random.rand(input_size, 10)
-predictions = mlp.predict(test_X)
-print("MLP predictions:", predictions)
+# predictions = mlp.predict(test_X)
+# print("MLP predictions:", predictions)
